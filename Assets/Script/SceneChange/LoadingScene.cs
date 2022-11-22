c19b70987a48d1a5aca5e2e9e3be1bda2295a1d1
C03940926 <C0394@oic.jp>
2022-11-22 16:21:29 +0900

    Revert "1-2-1 Map+"
    This reverts commit c2d4e2a0cfff7833efbd78f3a48b9904b5d04ebe.

--- Assets/Script/SceneChange/LoadingScene.cs
+++ Assets/Script/SceneChange/LoadingScene.cs
@@ -9,28 +9,31 @@ public class LoadingScene : MonoBehaviour
     [SerializeField] private GameObject _loadingUI;
     [SerializeField] private Slider _slider;
 
+    public float test;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
             LoadNextScene();
-        }
+        }        
     }
 
     public void LoadNextScene()
     {
         _loadingUI.SetActive(true);
         StartCoroutine(LoadScene());
-        
-        
     }
+
     IEnumerator LoadScene()
-    {
+    {        
         AsyncOperation async = SceneManager.LoadSceneAsync("GameScene");
         while (!async.isDone)
-        {
-            _slider.value = async.progress;
+        {            
+            _slider.value = async.progress;            
             yield return null;
         }
+
+        
     }
 }
